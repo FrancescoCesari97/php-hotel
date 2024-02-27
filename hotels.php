490a5896262f02a2095ad793aa8d1d0dca563011
FrancescoCesari97 <freedus97@gmail.com>
2024-02-27 17:06:00 +0100

    aggiunto filtro del parcheggio

--- hotels.php
+++ hotels.php
@@ -40,4 +40,35 @@
 
     ];
 
+ // * FILTRO PER LA RICERCA CON INPUT 
+    // $form_sent = !empty($_GET);
+
+    // $hotel_result = ($form_sent) ? [] : $hotels;
+
+    // if($form_sent){
+
+    //     $filter_name = $_GET['hotel-filter'] ?? '';
+
+    //     foreach($hotels as $hotel){
+    //         $result = false;
+    //         if(str_contains($hotel['name'], $filter_name))
+    //         {
+    //             $hotel_result[] = $hotel;
+    //         }
+    //     }
+    // }
+
+    // * FILTRO PER PARCHEGGIO
+
+    $filter_parcking = (isset($_GET['check-parking'])) ? true : false;
+
+    if($filter_parcking){
+
+        $hotels = array_filter($hotels, function($hotel, $index) {
+           return $hotel['parking'] == true ;
+       }, ARRAY_FILTER_USE_BOTH);
+    }
+
+
+
 ?>
--- index.php
+++ index.php
@@ -1,51 +1,88 @@
 <?php
 
- require_once "./hotels.php";
+ require "./hotels.php";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
+<title>Document</title>
+</head>
+
 <body>
     <div class="container">
 
-        <table class="table">
-          <thead>
-            <tr >
-              <th class="col-2">Name</th>
-              <th class="col-3">description</th>
-              <th class="col-2">parking</th>
-              <th class="col-2">vote</th>
-              <th class="col-3">distance to center</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($hotels as $hotel): ?>
-                
-                <tr>
+        <h1 class="text-center">Hotels</h1>
+        <div class=" card  my-5 ">
 
-                    <th> <?= $hotel["name"] ?></th>
-                    <td><?= $hotel["description"] ?></td>
-                    <td><?= $hotel['parking'] ? "Yes" : "No"; ?></td>
-                    <td><?= $hotel["vote"] ?></td>
-                    <td><?= $hotel["distance_to_center"] . "" . "km" ?></td>
+            <div class="card-body">
 
-                </tr>
-            <?php endforeach;  ?>
-            
-          </tbody>
-        </table>
+
+                <form method="GET" class=" d-flex align-items-center gap-3">
+                    <!-- <div>
+                        <input type="text" name="hotel-filter">
+
+                        <button class="  btn btn-primary"> Cerca</button>
+                    </div> -->
+                    <div class="form-check ">
+                        <input class="form-check-input" type="checkbox" value="" id="check-parking" name="check-parking"
+                            <?= $filter_parcking ? 'checked' : '' ?>>
+                        <label class="form-check-label" for="check-parking">
+                            con parcheggio
+                        </label>
+                    </div>
+
+                    <button class="  btn btn-primary"> Filtra</button>
+
+                </form>
+            </div>
+
+
+        </div>
+        <div class="card">
+            <div class="card-body">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="col-2">Name</th>
+                            <th class="col-3">description</th>
+                            <th class="col-2">parking</th>
+                            <th class="col-2">vote</th>
+                            <th class="col-3">distance to center</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($hotels as $hotel): ?>
+
+                        <tr>
+
+                            <th><?= $hotel["name"] ?></th>
+                            <td><?= $hotel["description"] ?></td>
+                            <td><?= $hotel['parking'] ? "Yes" : "No"; ?></td>
+                            <td><?= $hotel["vote"] ?></td>
+                            <td><?= $hotel["distance_to_center"] . "" . "km" ?></td>
+
+                        </tr>
+                        <?php endforeach;  ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </div>
-    
 
-   
+
+
 </body>
+
 </html>
